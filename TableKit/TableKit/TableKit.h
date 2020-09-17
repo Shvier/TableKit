6ef515e9195da6c44cdd35be8e20326594088c27
Shvier <Shvier1894@gmail.com>
2020-09-17 18:06:20 +0800

    [feat] init

---
+++ TableKit/TableKit/TableKit.h
@@ -0,0 +1,18 @@
+//
+//  TableKit.h
+//  TableKit
+//
+//  Created by Shvier on 2020/9/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TableKit.
+FOUNDATION_EXPORT double TableKitVersionNumber;
+
+//! Project version string for TableKit.
+FOUNDATION_EXPORT const unsigned char TableKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableKit/PublicHeader.h>
+
+
